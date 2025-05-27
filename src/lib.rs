1f782bd579a1b4a075edcd7eee3cb910f3fff0eb
Marat Bukharov <maratik@yandex-team.ru>
2025-05-27 18:22:50 +0300

    provide tcmalloc for ppc64le

--- src/lib.rs
+++ src/lib.rs
@@ -17,7 +17,12 @@
 
 #[cfg(not(all(
     target_os = "linux",
-    any(target_arch = "x86_64", target_arch = "aarch64")
+    any(
+        target_arch = "x86_64",
+        target_arch = "aarch64",
+        target_arch = "powerpc64"
+    ),
+    target_endian = "little"
 )))]
 mod mimalloc {
     pub(crate) use mimalloc::MiMalloc as BEMallocImpl;
@@ -27,7 +32,12 @@ mod mimalloc {
 }
 #[cfg(all(
     target_os = "linux",
-    any(target_arch = "x86_64", target_arch = "aarch64")
+    any(
+        target_arch = "x86_64",
+        target_arch = "aarch64",
+        target_arch = "powerpc64"
+    ),
+    target_endian = "little"
 ))]
 mod tcmalloc {
     pub(crate) use tcmalloc_better::TCMalloc as BEMallocImpl;
@@ -47,22 +57,42 @@ mod tcmalloc {
 
 #[cfg(not(all(
     target_os = "linux",
-    any(target_arch = "x86_64", target_arch = "aarch64")
+    any(
+        target_arch = "x86_64",
+        target_arch = "aarch64",
+        target_arch = "powerpc64"
+    ),
+    target_endian = "little"
 )))]
 use crate::mimalloc::BEMallocImpl;
 #[cfg(not(all(
     target_os = "linux",
-    any(target_arch = "x86_64", target_arch = "aarch64")
+    any(
+        target_arch = "x86_64",
+        target_arch = "aarch64",
+        target_arch = "powerpc64"
+    ),
+    target_endian = "little"
 )))]
 use crate::mimalloc::init_impl;
 #[cfg(all(
     target_os = "linux",
-    any(target_arch = "x86_64", target_arch = "aarch64")
+    any(
+        target_arch = "x86_64",
+        target_arch = "aarch64",
+        target_arch = "powerpc64"
+    ),
+    target_endian = "little"
 ))]
 use crate::tcmalloc::BEMallocImpl;
 #[cfg(all(
     target_os = "linux",
-    any(target_arch = "x86_64", target_arch = "aarch64")
+    any(
+        target_arch = "x86_64",
+        target_arch = "aarch64",
+        target_arch = "powerpc64"
+    ),
+    target_endian = "little"
 ))]
 use crate::tcmalloc::init_impl;
 use core::alloc::{GlobalAlloc, Layout};
